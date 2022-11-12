08dd02c607524086edce79a80eed341dc2226de3
Phatcharamon Namwayko <pnamwayk@z3t5c1.42bangkok.com>
2022-11-12 22:12:31 +0700

    rush00 finished

---
+++ Rush00/ex00/rush03.c
@@ -0,0 +1,41 @@
+
+void ft_putchar(char c);
+
+void    put_fmt(int row, int col, int x, int y)
+{
+    if (row == 0 && col == 0)
+        ft_putchar('A');
+    else if (row == 0 && col == y)
+        ft_putchar('C');
+    else if(row == x && col == 0)
+        ft_putchar('A');
+    else if(row == x && col == y)
+        ft_putchar('C');
+    else if (row == 0 || row == x)
+        ft_putchar('B');
+    else if (col == 0 || col == y)
+        ft_putchar('B');
+    else
+        ft_putchar(' ');
+}
+
+void    rush(int x, int y)
+{
+    int row;
+    int col;
+
+    row = 0;
+    if (x <= 0 || y <= 0)
+        return ;
+    while (row < x)
+    {
+        col = 0;
+        while (col < y)
+        {
+            put_fmt(row, col, x - 1, y - 1);
+            col++;
+        }
+        ft_putchar('\n');
+        row++;
+    }
+}
